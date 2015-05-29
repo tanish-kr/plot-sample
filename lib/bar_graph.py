1c19f80a7da89c41823a81f4446f8e8417eca9f1
tatsunori_nishikori <tatsunori_nishikori@metaps.com>
2015-05-29 17:48:25 +0900

    added bar chart

---
+++ lib/bar_graph.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+import json
+import urllib.request
+import matplotlib as mpl
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as tkr
+
+url = 'https://gist.githubusercontent.com/tatsu07/f1bc747e344b873bb836/raw/e650ed44f538a3f2b271c298bc4a1b98af4ba7f1/2015_Annual_income.json'
+
+response = urllib.request.urlopen(url)
+income_data = pd.read_json(response.read().decode('utf-8'))
+
+y_format = tkr.FuncFormatter(number_format)
+mpl.rcParams['font.family']='MS Gothic'
+
+# Tで行列を入れ替える
+income_data.T.plot(kind='bar', color=['g','r','b'])
+
+plt.title('年代別 年収')
+plt.ylabel("年収")
+plt.xlabel("年代")
+
+# 余白設定
+plt.gcf().subplots_adjust(bottom=0.15)
+
+plt.savefig('income_bar.png')
